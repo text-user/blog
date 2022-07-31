44eed52c420272a91bf025c0a22d68f4dac71f1e
text-user <69955122+text-user@users.noreply.github.com>
2022-08-01 08:24:22 +0900

    commit2

--- env.php
+++ env.php
@@ -8,14 +8,14 @@
 
 
 // heroku用の情報
-$url = parse_url(getenv("
-mysql://b7d1a928bfb1bb:e940d97c@us-cdbr-east-06.cleardb.net/heroku_028664bd7b01fd4?reconnect=true
-"));
+// $url = parse_url(getenv("
+// mysql://b7d1a928bfb1bb:e940d97c@us-cdbr-east-06.cleardb.net/heroku_028664bd7b01fd4?reconnect=true
+// "));git push heroku master
 
-define('DB_NAME', substr($url['path'], 1));
-define('DB_HOST', $url['host']);
-define('DB_USER', $url['user']);
-define('DB_PASS', $url['pass']);
+define('DB_NAME', 'blog_app');
+define('DB_HOST', 'us-cdbr-east-06.cleardb.net');
+define('DB_USER', 'b7d1a928bfb1bb@%');
+define('DB_PASS', 'b7d1a928bfb1bb');
 // ここまで
 
 
